4bdc3cb20ac8b5aa863fa74c5293b0261dd3435f
CYUZUZO010 <sandcyuzuzo@gmail.com>
2026-01-20 22:04:59 +0200

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,7 +6,7 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export function getPrisma(): PrismaClient {
   if (!globalForPrisma.prisma) {
-    // Lazy instantiate to allow consumers to catch initialization errors
+    
     globalForPrisma.prisma = new PrismaClient();
   }
   return globalForPrisma.prisma;
