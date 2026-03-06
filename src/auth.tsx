f69236a143ef3e147603610476fca004551051cd
CYUZUZO010 <sandcyuzuzo@gmail.com>
2026-03-06 21:53:53 +0200

    Clean up auth.tsx by removing comment
    Removed unnecessary comment from auth.tsx.

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,12 +1,10 @@
-//making the authentication
+
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
 export const {
   handlers: { GET, POST },
   auth,
-
-  
   signIn,
   signOut,
 } = NextAuth(
