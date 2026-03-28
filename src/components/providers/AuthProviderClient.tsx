0e3b04aef86e4a9d60aa8340d2767edaaa6ba6d2
CYUZUZO010 <sandcyuzuzo@gmail.com>
2026-03-28 23:53:43 +0200

    Add AuthProviderClient component

--- src/components/providers/AuthProviderClient.tsx
+++ src/components/providers/AuthProviderClient.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import { AuthProvider } from "@/context/AuthContext";
 export default function AuthProviderClient({ children }: { children: React.ReactNode }) {
