3d361a0464d43c76b316edf64b0bb4467ea07c3e
CYUZUZO010 <sandcyuzuzo@gmail.com>
2026-03-28 23:40:35 +0200

    update

--- src/components/providers/AuthProviderClient.tsx
+++ src/components/providers/AuthProviderClient.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { AuthProvider } from "@/context/AuthContext";
-
 export default function AuthProviderClient({ children }: { children: React.ReactNode }) {
   return <AuthProvider>{children}</AuthProvider>;
 }
